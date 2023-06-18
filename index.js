7749120ae9852354929a218b0afc6610ac2e89c9
voste126 <steveaustine126@gmail.com>
2023-06-18 15:56:19 +0300

    adding eventlisteners on DOM

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    let input = document.getElementById('button');
+    input.addEventListener('click',function(event){
+       return  alert('button clicked!')
+    })
 }
+addingEventListener()
